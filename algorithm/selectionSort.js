09abe0f764409cd6ca9af4aacb0da055dfea22a5
youpen <woo.yupeng@gmail.com>
2019-01-30 13:43:59 +0800

    alg: add selectionSort

---
+++ algorithm/selectionSort.js
@@ -0,0 +1,28 @@
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowestIndex = i;
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] < arr[lowestIndex]) {
+        lowestIndex = j;
+      }
+    }
+    swap(arr, i, lowestIndex)
+  }
+}
+
+function swap(arr, i, j){
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+
+const a = [];
+for (let i = 0; i < 10; i++) {
+  a.push(Math.ceil((Math.random() * Math.random()) * 100));
+}
+
+selectionSort(a);
+console.log(a);
+
