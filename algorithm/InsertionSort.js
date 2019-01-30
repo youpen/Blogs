8730a659e7ff56eac197b0df80c32216d3f38e24
youpen <woo.yupeng@gmail.com>
2019-01-30 14:28:02 +0800

    alg: add insertionSort

---
+++ algorithm/InsertionSort.js
@@ -0,0 +1,23 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (arr[j-1] > arr[j]) {
+        swap(arr, j-1, j);
+      }
+    }
+  }
+}
+
+function swap(arr, i, j){
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+const a = [];
+for (let i = 0; i < 10; i++) {
+  a.push(Math.ceil((Math.random() * Math.random()) * 100));
+}
+
+insertionSort(a);
+console.log(a);
